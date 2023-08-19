e9467597b09f17a302129906e5edb3a0134cd5ee
jeevanthi <jeevanthimovva9@gmail.com>
2023-08-19 21:55:24 +0530

    Class on 19/08

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Bird{
+    String color;
+    String legs;
+
+    void eat(){};
+    abstract void fly();
+
+}
+
+class sparrow extends Bird{
+
+    void eat(){
+        System .out.println("Sparrow eats");
+    }
+
+    @Override
+    void fly() {
+        System .out.println("Sparrow eats");
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        sparrow sparrow=new sparrow();
+        Bird bird=new Bird() {
+            @Override
+            void fly() {
+                System.out.println("this is abstract class");
+            }
+        };
+        sparrow.color="white";
+        sparrow.eat();
+        bird.fly();
+        System.out.println("Hello world!");
+
+       
+
+    }
+}
